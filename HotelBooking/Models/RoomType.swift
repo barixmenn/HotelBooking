596b7a28553bf8eda87652bed33aa6e974325884
baris dilekci <bdilekci200@gmail.com>
2023-07-17 17:28:24 +0300

    feat: interface created for room selection

--- HotelBooking/Models/RoomType.swift
+++ HotelBooking/Models/RoomType.swift
@@ -20,4 +20,12 @@ struct RoomType: Equatable{
     static func == (lhs: RoomType, rhs: RoomType) -> Bool  {
         return lhs.id == rhs.id
     }
+    
+    static var all : [RoomType] {
+        return [
+        RoomType(id: 0, name: "Two Queens", shortName: "2Q", price: 170),
+        RoomType(id: 1, name: "One King ", shortName: "K", price: 190),
+        RoomType(id: 2, name: "Suit", shortName: "S", price: 210)
+        ]
+    }
 }
---
+++ HotelBooking/Scene/SelectRoomTypeTableViewController.swift
@@ -0,0 +1,64 @@
+//
+//  SelectRoomTypeTableViewController.swift
+//  HotelBooking
+//
+//  Created by Baris on 17.07.2023.
+//
+
+import UIKit
+
+
+class SelectRoomTypeTableViewController: UITableViewController {
+
+    
+//MARK: - Properties
+    var selectedRoomType: RoomType?
+//MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+   
+
+}
+
+
+extension SelectRoomTypeTableViewController {
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+     
+        return RoomType.all.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "selectRoomCell", for: indexPath)
+        let roomType = RoomType.all[indexPath.row]
+        
+        cell.textLabel?.text = roomType.name
+        cell.detailTextLabel?.text = "$ \(roomType.price)"
+        
+        if roomType == selectedRoomType {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        selectedRoomType = RoomType.all[indexPath.row]
+        tableView.reloadData()
+    }
+
+}
